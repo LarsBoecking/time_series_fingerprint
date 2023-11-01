525caa180efe0a7c1604db6fd820c48b02a73ba2
lars.boecking <lars.boecking@fit.fraunhofer.de>
2023-11-01 20:12:36 +0100

    add univariate visualisation utils

--- src/utils_univariate.py
+++ src/utils_univariate.py
@@ -1,36 +1,70 @@
 from sktime.datasets._data_io import _load_provided_dataset
-import os 
+import os
 import pandas as pd
 
-DATA_PATH = os.path.join(os.getcwd(), "datasets", "Univariate_ts")
+UNIVARIATE_DATA_PATH = os.path.join(os.getcwd(), "datasets", "Univariate_ts")
 
-def _load_data_set(data_set_name = "ArrowHead"):
+
+def _load_data_set(data_set_name="ArrowHead"):
     """
     Load the specified data set.
-    
+
     Args:
         data_set_name (str): The name of the data set to load. Default is "ArrowHead".
-        
+
     Returns:
         train_data (pandas.DataFrame): The training data set.
         test_data (pandas.DataFrame): The test data set.
     """
 
-    train_data = _load_provided_dataset(name=data_set_name,split="train", return_X_y=False, return_type=None, extract_path=DATA_PATH
-                                )
+    train_data = _load_provided_dataset(
+        name=data_set_name,
+        split="train",
+        return_X_y=False,
+        return_type=None,
+        extract_path=UNIVARIATE_DATA_PATH,
+    )
 
-    test_data = _load_provided_dataset(name=data_set_name,split="test", return_X_y=False, return_type=None, extract_path=DATA_PATH
-                                )
-
+    test_data = _load_provided_dataset(
+        name=data_set_name,
+        split="test",
+        return_X_y=False,
+        return_type=None,
+        extract_path=UNIVARIATE_DATA_PATH,
+    )
 
     print(f"Data set train instances: {train_data.shape[0]}")
     print(f"Train targets: {train_data.iloc[:,1].value_counts().to_dict()}")
-    train_instance_length = [train_data.iloc[instance, 0].shape[0] for instance in range(len(train_data))]
-    print(f"Train instance lengths: {pd.DataFrame(train_instance_length).value_counts().to_dict()}\n")
+    train_instance_length = [
+        train_data.iloc[instance, 0].shape[0] for instance in range(len(train_data))
+    ]
+    print(
+        f"Train instance lengths: {pd.DataFrame(train_instance_length).value_counts().to_dict()}\n"
+    )
 
     print(f"Data set test instances: {test_data.shape[0]}")
     print(f"Test targets: {test_data.iloc[:,1].value_counts().to_dict()}")
-    test_instance_length = [test_data.iloc[instance, 0].shape[0] for instance in range(len(train_data))]
-    print(f"Test instance lengths: {pd.DataFrame(test_instance_length).value_counts().to_dict()}")
-    
+    test_instance_length = [
+        test_data.iloc[instance, 0].shape[0] for instance in range(len(test_data))
+    ]
+    print(
+        f"Test instance lengths: {pd.DataFrame(test_instance_length).value_counts().to_dict()}"
+    )
+
     return train_data, test_data
+
+
+
+def list_univariate_data_sets(path=UNIVARIATE_DATA_PATH):
+    """
+    List all univariate data sets in the defined path.
+
+    Args:
+        path (str): The path to search for subfolders.
+
+    Returns:
+        subfolders (list): A list of subfolder names representing data sets.
+    """
+    subfolders = [f.name for f in os.scandir(path) if f.is_dir()]
+    
+    return subfolders
---
+++ src/utils_visualization.py
@@ -0,0 +1,40 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+# make sure outputs are reproducable
+np.random.seed(42)
+
+# define default visualization values
+FIGURE_SIZE = (15, 8)
+TITLE_FONT_SIZE = 20
+AXIS_LABEL_FONT_SIZE = 16
+AXIS_TICKS_FONT_SIZE = 16
+LEGEND_FONT_SIZE = 16
+
+
+def _get_image_path(PROJECT_ROOT_DIR, CHAPTER_ID):
+    IMAGES_PATH = os.path.join(os.getcwd(), "images", CHAPTER_ID)
+    os.makedirs(IMAGES_PATH, exist_ok=True)
+
+    return IMAGES_PATH
+
+
+class NotebookFigureSaver:
+    def __init__(self, CHAPTER_ID) -> None:
+        self.PROJECT_ROOT_DIR = os.getcwd()
+        self.IMAGES_PATH = _get_image_path(self.PROJECT_ROOT_DIR, CHAPTER_ID)
+        self.FIGURE_SIZE = FIGURE_SIZE
+        self.TITLE_FONT_SIZE = TITLE_FONT_SIZE
+        self.AXIS_LABEL_FONT_SIZE = AXIS_LABEL_FONT_SIZE
+        self.AXIS_TICKS_FONT_SIZE = AXIS_TICKS_FONT_SIZE
+        self.LEGEND_FONT_SIZE = LEGEND_FONT_SIZE
+
+    # function to save matplots to images folder
+    def save_fig(self, fig_id, tight_layout=True, fig_extension="png", resolution=300):
+        path = os.path.join(self.IMAGES_PATH, fig_id + "." + fig_extension)
+        print("Saving figure", fig_id)
+        if tight_layout:
+            plt.tight_layout()
+        plt.savefig(path, format=fig_extension, dpi=resolution)
