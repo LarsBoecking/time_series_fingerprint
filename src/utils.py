eddb47166955921ae34bd522f9078024489bf711
leopold.johann.mueller <leopold.johann.mueller@fit.fraunhofer.de>
2023-11-14 07:44:14 +0100

    normalize data sets

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import os
 from typing import Tuple
 import pandas as pd
 from sktime.datasets._data_io import _load_provided_dataset
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+import numpy as np
 
 # Define the data paths
 MULTIVARIATE_DATA_PATH = os.path.join(os.getcwd(), "datasets", "Multivariate_ts")
@@ -24,9 +26,11 @@ def list_data_sets(multivariate: bool = True):
     
     return subfolders
 
+
 def _load_data_set(
-    data_set_name: str = "ArticularyWordRecognition",
-    multivariate: bool = True,
+    data_set_name: str = "Beef",
+    multivariate: bool = False,
+    normalization: str = "minmax"  # Default set to 'none' for no normalization
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """
     Load a specific data set with the predefined train test split from sk-time.
@@ -34,16 +38,15 @@ def _load_data_set(
     Parameters:
         data_set_name (str): The name of the data set to load. Defaults to "ArticularyWordRecognition".
         multivariate (bool): Whether the data set is multivariate or univariate. Defaults to True.
-        debugging_information (bool): Whether to print debugging information. Defaults to False.
+        normalization (str): The type of normalization to apply. Options are 'none', 'minmax', 'zscore', 'mean', 'robust', 'unit_vector'.
+                              Defaults to 'none' for no normalization.
 
     Returns:
-        tuple: A tuple containing the train data and test data.
+        tuple: A tuple containing the train data and test data, possibly normalized.
     """
     # Determine the extract path based on whether the data set is multivariate or univariate
     extract_path = MULTIVARIATE_DATA_PATH if multivariate else UNIVARIATE_DATA_PATH
     
-    #! normalize data set, such that the embedding is comparable
-
     # Load the train data set
     train_data = _load_provided_dataset(
         name=data_set_name,
@@ -52,8 +55,6 @@ def _load_data_set(
         return_type=None,
         extract_path=extract_path,
     )
-    
-    #! normalize to have values between 0 and 1 based on train data 
 
     # Load the test data set
     test_data = _load_provided_dataset(
@@ -63,6 +64,45 @@ def _load_data_set(
         return_type=None,
         extract_path=extract_path,
     )
-    
+
+    # Apply normalization only if required
+    if normalization != "none":
+        if normalization == "minmax":
+            scaler = MinMaxScaler()
+        elif normalization == "zscore":
+            scaler = StandardScaler()
+        elif normalization == "mean":
+            scaler = MinMaxScaler(feature_range=(-1, 1))
+        elif normalization == "robust":
+            scaler = RobustScaler()
+        elif normalization == "unit_vector":
+            scaler = lambda x: x / np.linalg.norm(x)
+
+        # Apply normalization to train and test data
+        for col in train_data.columns:
+            if col.startswith('dim_'):
+                # Concatenate all series in the column
+                concatenated_train = pd.concat(train_data[col].tolist())
+                concatenated_test = pd.concat(test_data[col].tolist())
+
+                if normalization != "unit_vector":
+                    # Normalize the concatenated series
+                    concatenated_train = scaler.fit_transform(concatenated_train.values.reshape(-1, 1)).ravel()
+                    concatenated_test = scaler.transform(concatenated_test.values.reshape(-1, 1)).ravel()
+                else:
+                    concatenated_train = scaler(concatenated_train)
+                    concatenated_test = scaler(concatenated_test)
+
+                # Split the series back to the original format
+                split_indices = train_data[col].apply(len).cumsum()
+                train_data[col] = np.split(concatenated_train, split_indices[:-1])
+                train_data[col] = [pd.Series(data) for data in train_data[col]]
+
+                split_indices = test_data[col].apply(len).cumsum()
+                test_data[col] = np.split(concatenated_test, split_indices[:-1])
+                test_data[col] = [pd.Series(data) for data in test_data[col]]
+
     # Return the train and test data sets
     return train_data, test_data
+
+# %%
